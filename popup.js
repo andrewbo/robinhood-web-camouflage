f5778464d0df915b987f0e332220657a7dd6a7cd
Andrey Bocharov <andrey.n.bocharov@gmail.com>
2018-04-25 17:11:25 -0500

    Init.

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.getElementById('go-to-options').onclick = function() {
+    if (chrome.runtime.openOptionsPage) {
+        chrome.runtime.openOptionsPage();
+    } else {
+        window.open(chrome.runtime.getURL('options.html'));
+    }
+};
